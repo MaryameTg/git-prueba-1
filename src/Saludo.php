014c8b582f6ccb88bb3a3ffe5fff7d1f39ae6a3a
Maryame Tghos Naim <maryametghos@gmail.com>
2021-11-23 13:11:59 +0100

    Añadida funcionalidad de saludo

---
+++ src/Saludo.php
@@ -0,0 +1,18 @@
+<?php
+
+class Saludo
+{
+
+    private $saludo;
+    function __construct($name)
+    {
+
+        $this->saludo = "Hola $name";
+    }
+
+    function saludar()
+    {
+
+        echo $this->saludo;
+    }
+}
--- src/index.php
+++ src/index.php
@@ -1,3 +1,6 @@
 <?php
 
 echo "Hola mundo de Git";
+$saludo=new Saludo("Fermin");
+
+$saludo->saludar();
